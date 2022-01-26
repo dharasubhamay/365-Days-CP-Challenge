f6430dea55e04c17e610415c01157aa24a59c2ef
dharasubhamay <82009535+dharasubhamay@users.noreply.github.com>
2022-01-26 23:42:01 +0530

    Update d9_linkedListCycle.cpp

--- d9_linkedListCycle.cpp
+++ d9_linkedListCycle.cpp
@@ -12,7 +12,7 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if(head == NULL && head->next == NULL)
+        if(head == NULL || head->next == NULL)
             return false;
         ListNode *fastNode = head;
         ListNode *slowNode = head;
